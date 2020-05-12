1e29964449a6d745e8c39bc297534ea008403048
yangb03 <yangb03@mysoft.com.cn>
2020-05-12 13:27:35 +0800

    修改敌机爆炸逻辑

--- games/games/plan-war/js/enemy-plan.js
+++ games/games/plan-war/js/enemy-plan.js
@@ -6,24 +6,15 @@
      */
     const EnemyPlan = window.EnemyPlan = Base.extend({
         init: function(bullet) {
-
             this._super();
-            //子弹对象
-            this.bullet = bullet;
-
-            this.canvas = document.getElementById("enemyCanvas");
-            this.ctx = this.canvas.getContext("2d");
-            this.speed = 100;
-            this.timer = null;
-            this.resetCanvasSize();
-
+    
             //敌机资源信息
             this.enemyResourcesArray = [
                 {
                     src: "./static/img/enemy0.png",
                     blastResourcePrefix: "./static/img/enemy0_down",
                     blastStep: 4, //爆炸有4个图片 索引从1开始
-                    blastCurrentSetp: -1, //当前爆炸步骤
+                    blastCurrentStep: -1, //当前爆炸步骤
                     w: 51,
                     h: 39,
                     needBullet: 1, //打爆该敌机需要的子弹数
@@ -32,7 +23,7 @@
                     src: "./static/img/enemy1.png",
                     blastResourcePrefix: "./static/img/enemy1_down",
                     blastStep: 4, //爆炸有4个图片 索引从1开始
-                    blastCurrentSetp: -1, //当前爆炸步骤
+                    blastCurrentStep: -1, //当前爆炸步骤
                     w: 69,
                     h: 89,
                     needBullet: 2
@@ -40,15 +31,29 @@
                 {
                     src: "./static/img/enemy2.png",
                     blastResourcePrefix: "./static/img/enemy2_down",
-                    blastStep: 6, //爆炸有4个图片 索引从1开始
-                    blastCurrentSetp: -1, //当前爆炸步骤
+                    blastStep: 6, //爆炸有6个图片 索引从1开始
+                    blastCurrentStep: -1, //当前爆炸步骤
                     w: 165,
                     h: 246,
                     needBullet: 3
                 }
             ];
+            this.enemyResourcesCount = 17;
+            this.enemyResourcesHadLoadedCount = 0;
+            this.enemyResourcesHadLoaded = false;
             //存放敌机数量数组
             this.enemysArray = [];
+            
+            //初始化所有的敌机图片资源
+            this.initLoadAllResource();
+            
+            //子弹对象
+            this.bullet = bullet;
+            this.canvas = document.getElementById("enemyCanvas");
+            this.ctx = this.canvas.getContext("2d");
+            this.speed = 100;
+            this.timer = null;
+            this.resetCanvasSize();
 
             this.timerForAddEnemy = null;
             this.speedForAddEnemy = 2000;
@@ -59,6 +64,29 @@
             img.src = this.enemyResourcesArray[0].src;
             img.onload = () => {};
             this.img = img;
+        },
+        //初始加载所有图片资源
+        initLoadAllResource() {
+            this.enemyResourcesArray.map(item => {
+                const img = new Image()
+                img.src = item.src;
+                img.onload = () => {
+                    this.enemyResourcesHadLoadedCount ++;
+                    if (this.enemyResourcesCount === this.enemyResourcesHadLoadedCount) {
+                        this.enemyResourcesHadLoaded = true;
+                    }
+                }
+                for (let i = 1; i <= item.blastStep; i++) {
+                    const blastImg = new Image();
+                    blastImg.src = item.blastResourcePrefix + i + '.png'
+                    blastImg.onload = () => {
+                        this.enemyResourcesHadLoadedCount ++;
+                        if (this.enemyResourcesCount === this.enemyResourcesHadLoadedCount) {
+                            this.enemyResourcesHadLoaded = true;
+                        }
+                    }
+                }
+            })
         },
         // 获取给定整数区间的随机数
         getRandomNumber(min, max) {
@@ -101,13 +129,13 @@
                             enemy.bulletId.push(bullet.id);
                             const len = enemy.bulletId.length;
                             if (len > 0 && len >= enemy.needBullet) {
-                                enemy.isBlast = true;
-                                this.enemyBlast();
+                                enemy.canBlast = true;
                             }
                         }
                     }
                 });
             });
+            this.enemyBlast();
         },
         //添加敌机
         enemyAdd: function() {
@@ -123,31 +151,40 @@
                 bulletId: [], //击中该敌机的子弹
                 needBullet: resource.needBullet,
                 blastResourcePrefix: resource.blastResourcePrefix,
-                blastStep: resource.blastStep, 
-                blastCurrentSetp: resource.blastCurrentSetp,
-                isBlast: false,
+                blastStep: resource.blastStep,
+                blastCurrentStep: resource.blastCurrentStep,
+                blastNeedLoop: 5,    //爆炸需要的轮数为5次
+                blastCurrentLoop: 0, //爆炸当前的轮数
+                canBlast: false,   //能爆炸
             })
         },
         //敌机被打中爆炸
         enemyBlast: function() {
-            const blastEnemyArr = this.enemysArray.filter(enemy => enemy.isBlast);
-            blastEnemyArr.map(enemy => {
-                setInterval(() => {
-                    if (enemy.blastCurrentSetp === -1) {
-                        enemy.blastCurrentSetp = 1;
-                    }
-                    enemy.src = `${enemy.blastResourcePrefix}${enemy.blastCurrentSetp}.png`
-                    enemy.blastCurrentSetp ++;
-                }, 24)
+            this.enemysArray.map((enemy, index) => {
+                if (enemy.canBlast) {
+                    setInterval(() => {
+                        if (enemy.blastCurrentStep === -1) {
+                            enemy.blastCurrentStep = 1;
+                        }
+                        enemy.src = `${enemy.blastResourcePrefix}${enemy.blastCurrentStep}.png`
+                        enemy.blastCurrentStep ++;
+                        if (enemy.blastCurrentStep > enemy.blastStep) {
+                            enemy.blastCurrentStep = 1
+                            if (enemy.blastCurrentLoop > enemy.blastNeedLoop) {
+                                enemy.x = -enemy.w;
+                                enemy.blastCurrentLoop = enemy.blastNeedLoop;
+                            }
+                            enemy.blastCurrentLoop ++;
+                        }
+                    }, 24)
+                }
             })
         },
         updatePosition: function() {
             this.enemysArray.map((enemy, index) => {
-                if (!enemy.isBlast) {
-                    enemy.y += 10
-                    if (enemy.y > this.canvas.height + enemy.h) {
-                        this.enemysArray.splice(index, 1)
-                    }
+                enemy.y += 10
+                if (enemy.y > this.canvas.height + enemy.h) {
+                    this.enemysArray.splice(index, 1)
                 }
                 return enemy
             })
@@ -161,13 +198,17 @@
         },
         render: function() {
             this.timerForAddEnemy = setInterval(() => {
-                this.enemyAdd()
+                if (this.enemyResourcesHadLoaded) {
+                    this.enemyAdd()
+                }
             }, this.speedForAddEnemy);
 
             this.timer = setInterval(() => {
-                this.isHit();
-                this.updatePosition();
-                this.move();
+                if (this.enemyResourcesHadLoaded) {
+                    this.isHit();
+                    this.updatePosition();
+                    this.move();
+                }
             }, this.speedForMove)
         }
     });
